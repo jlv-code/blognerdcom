262f43edde6d2ac592362d6b39bf9448facec334
José Viéitez <jlvieitez@gmail.com>
2018-12-24 10:55:42 -0500

    Blog advances

--- wp-content/themes/nerdcomblog/partials/back-to-home.php
+++ wp-content/themes/nerdcomblog/partials/back-to-home.php
@@ -1,5 +1,5 @@
 <div class="row no-gutters">
     <div class="container">
-        <a href="<?php echo get_bloginfo('url') ?>" class="btn btn-sm text-gray-600"><i class="fa fa-fw fa-arrow-left"></i> Volver al Inicio</a>
+        <a href="<?php echo get_bloginfo('url') ?>" class="btn btn-sm btn-back"><i class="fa fa-fw fa-arrow-left"></i> Volver al Inicio</a>
     </div>
 </div>
